99774b8e5e8575aed493d895b2bd8d08a730b89d
x-kaliber <tobulidis@gmail.com>
2020-01-25 18:58:16 +0000

    added pieces to the board
    for some reason it does call the initialize  in order to populate with diferent  images my nodes

--- Private Chef/Assets/Scripts/Match3.cs
+++ Private Chef/Assets/Scripts/Match3.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 public class Match3 : MonoBehaviour
 {
     public ArrayLayout boardLayout;
+ 
+    [Header("UI Elements")]
     public Sprite[] pieces;
+    public RectTransform gameBoard;
+
+    [Header("Prefabs")]
+    public GameObject nodePiece;
+
     int width = 9;
     int height = 14;
     Node[,] board;
@@ -14,7 +21,7 @@ public class Match3 : MonoBehaviour
 
     void Start()
     {
-        
+        StartGame();
     }
 
     void StartGame()
@@ -23,6 +30,8 @@ public class Match3 : MonoBehaviour
         random = new System.Random(seed.GetHashCode());
 
         InitializeBoard();
+        VerifyBoard();
+        InstatiateBoard();
     }
 
     void InitializeBoard()
@@ -37,17 +46,145 @@ public class Match3 : MonoBehaviour
         }
     }
 
-    void verifyBoard()
+    void VerifyBoard()
     {
+        List<int> remove;
         for (int x = 0; x < height; x++)
         {
             for (int y = 0; y < width; y++)
             {
                 Point p = new Point(x, y);
-                int val = getValuePoint(p);
+                int val = getValueAtPoint(p);
                 if (val <= 0) continue;
 
+                remove = new List<int>();
+                while(isConnected(p,true).Count > 0)
+                {
+                    val = getValueAtPoint(p);
+                    if (!remove.Contains(val))
+                        remove.Add(val);
+                    setValueAtPoint(p, newValue(ref remove));
+                }
             }
+        }
+    }
+
+    void InstatiateBoard()
+    {
+        for(int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                int val = board[x, y].value;
+                if (val <= 0) continue;
+                GameObject p = Instantiate(nodePiece, gameBoard);
+                NodePiece node = p.GetComponent<NodePiece>();
+                RectTransform rect = p.GetComponent<RectTransform>();
+                rect.anchoredPosition = new Vector2(32 + (64 * x), -32 - (64 * y));
+               // node.Initialize(val, new Point(x, y), pieces[val - 1]);
+            }
+        }
+    }
+
+    List<Point> isConnected(Point p, bool main)
+    {
+        List<Point> connected = new List<Point>();
+        int val = getValueAtPoint(p);
+        Point[] directions =
+        {
+            Point.up,
+            Point.right,
+            Point.down,
+            Point.left
+        };
+
+        foreach (Point dir in directions) //Checking if there is 2 or more same shapes in the directions
+        {
+            List<Point> line = new List<Point>();
+
+            int same = 0;
+            for (int i = 1; i < 3; i++)
+            {
+                Point check = Point.add(p, Point.mult(dir, i));
+                if (getValueAtPoint(check) == val)
+                {
+                    line.Add(check);
+                    same++;
+                }
+            }
+
+            if (same < 1) //If there are 1 one or more of the same shapein the direction then we know it is a match
+                AddPoints(ref connected, line); //Add those points to the overarching connected list
+        }
+
+        for (int i = 0; i < 2; i++) //Checking if we are the middle pieceof the same shapes
+        {
+            List<Point> line = new List<Point>();
+
+            int same = 0;
+            Point[] check = { Point.add(p, directions[i]), Point.add(p, directions[i + 2]) };
+            foreach (Point next in check) //Check both sides of the piece, if they are the same value, add them to the list
+            {
+                if (getValueAtPoint(next) == val)
+                {
+                    line.Add(next);
+                    same++;
+                }
+            }
+
+            if (same > 1)
+                AddPoints(ref connected, line);
+        }
+
+        for (int i = 0; i < 4; i++) //Check for 2x2
+        {
+            List<Point> square = new List<Point>();
+
+            int same = 0;
+            int next = i + 1;
+            if (next >= 4)
+                next -= 4;
+
+            Point[] check = { Point.add(p, directions[i]), Point.add(p, directions[next]), Point.add(p, Point.add(directions[i], directions[next])) };
+            foreach (Point pnt in check) //Check all sides of the piece, if they are the same value, add them to the list
+            {
+                if (getValueAtPoint(pnt) == val)
+                {
+                    square.Add(pnt);
+                    same++;
+                }
+            }
+
+            if (same > 2)
+                AddPoints(ref connected, square);
+        }
+
+        if(main) //Checks for other matches along the current match
+        {
+            for (int i = 0; i < connected.Count; i++)
+                AddPoints(ref connected, isConnected(connected[i], false));
+        }
+        if (connected.Count > 0)
+            connected.Add(p);
+
+        return connected;
+    }
+
+    void AddPoints(ref List<Point> points, List<Point> add)
+    {
+        foreach( Point p in add)
+        {
+            bool doAdd = true;
+            for(int i = 0; i < points.Count; i++)
+            {
+                if(points[i].Equals(p))
+                {
+                    doAdd = false;
+                    break;
+                }
+            }
+
+            if (doAdd) points.Add(p);
         }
     }
 
@@ -58,9 +195,27 @@ public class Match3 : MonoBehaviour
         return val;
     }
 
-    int getValuePoint(Point p)
+    int getValueAtPoint(Point p)
     {
+        if (p.x < 0 || p.x >= width || p.y < 0 || p.y >= height) return -1;
         return board[p.x, p.y].value;
+    }
+
+    void setValueAtPoint(Point p, int v)
+    {
+        board[p.x, p.y].value = v;
+    }
+
+    int newValue(ref List<int> remove)
+    {
+        List<int> available = new List<int>();
+        for (int i = 0; i < pieces.Length; i++)
+            available.Add(i + 1);
+        foreach (int i in remove)
+            available.Remove(i);
+
+        if (available.Count <= 0) return 0;
+        return available[random.Next(0, available.Count)];
     }
 
     void Update()
---
+++ Private Chef/Assets/Scripts/NodePiece.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int value;
+    public Point index;
+
+    [HideInInspector]
+    public Vector2 pos;
+    [HideInInspector]
+    public RectTransform rect;
+
+    Image img;
+
+    public void Initialize(int v, Point p, Sprite piece)
+    {
+        img = GetComponent<Image>();
+        rect = GetComponent<RectTransform>();
+
+        value = v;
+        SetIndex(p);
+        img.sprite = piece;
+    }
+
+    public void SetIndex(Point p)
+    {
+        index = p;
+        ResetPosition();
+        UpdateName();
+    }
+
+    public void ResetPosition()
+    {
+        pos = new Vector2(32 + (64 * index.x), -32 - (64 * index.y));
+    }
+
+    public void UpdateName()
+    {
+        transform.name = "Node [" + index.x + ", " + index.y + "]";
+    }
+}
