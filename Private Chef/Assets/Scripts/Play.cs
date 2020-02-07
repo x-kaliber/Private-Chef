db8078f98a1f891dad90658e3564447d6172b2e0
x-kaliber <tobulidis@gmail.com>
2020-02-07 17:13:09 +0000

    main menu, Credits
    *** We have made a main menu.
    *** We connected the game to the main menu
    *** We made and connected the credit scene
    *** We made and connected the exit button in the main menu

---
+++ Private Chef/Assets/Scripts/Play.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+  public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void CreditGame()
+    {
+        SceneManager.LoadScene("Credit");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log ("Quit");
+        Application.Quit();
+    }
+}
