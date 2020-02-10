6fec56e18e25d635f8f60615882a086bb905d488
x-kaliber <tobulidis@gmail.com>
2020-02-10 11:54:52 +0000

    Minor fixes
    *** Fixed Home Button in main game (second text)
    *** Fixed positions of  MainChar Button in Main Game
    *** Fixed positions of  AssistantChar Button in Main Game
    *** Fixed positions of  Inventory Button in Main Game

--- Private Chef/Assets/Scripts/Play.cs
+++ Private Chef/Assets/Scripts/Play.cs
@@ -5,32 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class Play : MonoBehaviour
 {
-  public void PlayGame()
+
+  public void PlayGame() // We call the main Game
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void CreditGame()
+    public void CreditGame() // We call the Credit Scene
     {
         SceneManager.LoadScene("Credit");
     }
 
-    public void RecipeBook()
+    public void RecipeBook() // We call the Recipe Book Scene
     {
         SceneManager.LoadScene("Recipes");
     }
 
-    public void Mall()
+    public void Mall() // We call the Store/Mall scene
     {
         SceneManager.LoadScene("Store");
     }
 
-    public void OptionsMenu()
+    public void OptionsMenu() // We call Options Menu Scene
     {
         SceneManager.LoadScene("Options");
     }
 
-    public void QuitGame()
+    public void QuitGame() // We Quit the game
     {
         Debug.Log ("Quit");
         Application.Quit();
